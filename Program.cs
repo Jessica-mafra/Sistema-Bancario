e5c767d22aa7f6eba2d90713149a3c83396e6104
Jessica-mafra <119601801+Jessica-mafra@users.noreply.github.com>
2023-02-03 13:19:38 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿namespace SistemaBancario
+{
+    public class SistemaBancaro
+    {
+        public static void Main(string[]args)
+        {
+            var conta = new Conta();
+            conta.NomeConta = " Conta Teste";
+            conta.NumeroConta = 1000;
+            conta.Agencia = 001;
+            conta.Saldo = 1000;
+
+            conta.Titular = new Titular()
+            {
+                CPF = "00364555879",
+                RG = "54696457",
+                Endereco = "Rua Centro",
+                Nome = "Luiza",
+            };
+
+            Console.WriteLine($"O saldo atual é de : { conta.VerSaldo()}");
+
+            conta.Depositar(150);
+
+            Console.WriteLine($"O saldo atual, após o depósito é : {conta.VerSaldo()}");
+
+            conta.Sacar(900);
+
+            Console.WriteLine($"O saldo atual, após o saque é : {conta.VerSaldo()} ");
+
+            conta.Sacar(2000);
+            Console.WriteLine("Seu saldo é indisponivel para saque");
+        }
+    }
+}
---
+++ Titular.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaBancario
+{
+    internal class Titular
+    {
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public string RG { get; set; }
+        public string Endereco { get; set; }
+
+    }
+}
